ccdfdaf7d53676cf4535c2642d3a89009294a2a3
iagoa <iagoantunes.f@gmail.com>
2021-12-29 08:26:09 -0300

    Modelo e Armazenamento

---
+++ AppListaDeTarefas/AppListaDeTarefas/Modelos/GerenciadorTarefa.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppListaDeTarefas.Modelos
+{
+    public class GerenciadorTarefa
+    {
+        private List<Tarefa> Lista { get; set; }
+        public void Finalizar(int Index,Tarefa tarefa)
+        {
+            Lista.RemoveAt(Index);
+            Lista.Add(tarefa);
+
+            salvarProperties(Lista);
+
+        }
+        public void Salvar(Tarefa tarefa)
+        {
+            Lista.Add(tarefa);
+            salvarProperties(Lista);
+
+        }
+        public void Remover(Tarefa tarefa)
+        {
+            Lista.Remove(tarefa);
+
+            salvarProperties(Lista);
+
+        }
+        public List<Tarefa> Listagem()
+        {
+
+            return ListagemNoProperties();
+        }
+        private void salvarProperties(List<Tarefa> Lista)
+        {
+            if (App.Current.Properties.ContainsKey("Tarefas"))
+            {
+                App.Current.Properties.Remove("Tarefas");
+
+            }
+            App.Current.Properties.Add("Tarefas", Lista);
+        }
+        private List<Tarefa> ListagemNoProperties()
+        {
+            if (App.Current.Properties.ContainsKey("Tarefas"))
+            {
+                return (List<Tarefa>)App.Current.Properties["Tarefas"];
+
+            }
+            return new List<Tarefa>();
+        }
+
+    }
+}
---
+++ AppListaDeTarefas/AppListaDeTarefas/Modelos/Tarefa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppListaDeTarefas.Modelos
+{
+    public class Tarefa
+    {
+        public string Nome { get; set; }
+        public DateTime dataFinalizacao { get; set; }
+        public byte Prioridade { get; set; }
+
+
+
+
+
+    }
+}
