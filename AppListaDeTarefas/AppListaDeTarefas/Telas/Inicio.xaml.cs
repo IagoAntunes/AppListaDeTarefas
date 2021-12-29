77a28091c172012d51c92e258ecc44b674ccd320
iagoa <iagoantunes.f@gmail.com>
2021-12-29 10:33:19 -0300

    Tela "Inicio" - Criando Listagem - Parte 1

--- AppListaDeTarefas/AppListaDeTarefas/Telas/Inicio.xaml.cs
+++ AppListaDeTarefas/AppListaDeTarefas/Telas/Inicio.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AppListaDeTarefas.Modelos;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,10 +16,42 @@ namespace AppListaDeTarefas.Telas
         public Inicio()
         {
             InitializeComponent();
+            DataHoje.Text = DateTime.Now.DayOfWeek.ToString() + "," + DateTime.Now.ToString("dd/MM");
         }
         public void ActionGoCadastro(object sender,EventArgs args)
         {
             Navigation.PushAsync(new Cadastro());
+        }
+        public void LinhaStackLayout(Tarefa tarefa)
+        {
+
+            Label txtTarefa = new Label()
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Text = tarefa.Nome
+            };
+
+            Image Check = new Image()
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Source = ImageSource.FromFile("CheckOff.png")
+            };
+
+            if(Device.RuntimePlatform == Device.UWP)
+            {
+                Check.Source = ImageSource.FromFile("Resources/CheckOff.png");
+            }
+
+            StackLayout linha = new StackLayout()
+            {
+                Orientation = StackOrientation.Horizontal,
+                Spacing = 15
+            };
+
+            linha.Children.Add(Check);
+
+
         }
     }
 }
