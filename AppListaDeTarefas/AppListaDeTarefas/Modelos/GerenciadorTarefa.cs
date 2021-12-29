19c71f4e896f64555091ac265e8e4acb1234d06a
iagoa <iagoantunes.f@gmail.com>
2021-12-29 10:07:27 -0300

    Salvar Tarefa

--- AppListaDeTarefas/AppListaDeTarefas/Modelos/GerenciadorTarefa.cs
+++ AppListaDeTarefas/AppListaDeTarefas/Modelos/GerenciadorTarefa.cs
@@ -9,6 +9,7 @@ namespace AppListaDeTarefas.Modelos
         private List<Tarefa> Lista { get; set; }
         public void Finalizar(int Index,Tarefa tarefa)
         {
+            Lista = Listagem();
             Lista.RemoveAt(Index);
             Lista.Add(tarefa);
 
@@ -17,12 +18,14 @@ namespace AppListaDeTarefas.Modelos
         }
         public void Salvar(Tarefa tarefa)
         {
+            Lista = Listagem();
             Lista.Add(tarefa);
             salvarProperties(Lista);
 
         }
         public void Remover(Tarefa tarefa)
         {
+            Lista = Listagem();
             Lista.Remove(tarefa);
 
             salvarProperties(Lista);
--- AppListaDeTarefas/AppListaDeTarefas/Telas/Cadastro.xaml.cs
+++ AppListaDeTarefas/AppListaDeTarefas/Telas/Cadastro.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using AppListaDeTarefas.Modelos;
 
 namespace AppListaDeTarefas.Telas
 {
@@ -16,6 +17,7 @@ namespace AppListaDeTarefas.Telas
         {
             InitializeComponent();
         }
+        private byte Prioridade { get; set; }
         public void PrioridadeSelectAction(object sender,EventArgs args)
         {
             var Stacks = SLPrioridades.Children;
@@ -25,9 +27,40 @@ namespace AppListaDeTarefas.Telas
                 lblPrioridade.TextColor = Color.Gray;
             }
             ((Label)((StackLayout)sender).Children[1]).TextColor = Color.White;
-            FileImageSource Source = ((Image)((StackLayout)sender).Children[1]).Source as FileImageSource;
-            string prioridade = Source.File.ToString().Replace("Resources/","").Replace(".png","");
-            TxtNome.Text = prioridade;
+            FileImageSource Source = ((Image)((StackLayout)sender).Children[0]).Source as FileImageSource;
+            string Prioridade = Source.File.ToString().Replace("Resources/p","").Replace(".png","");
+
+            this.Prioridade = byte.Parse(Prioridade);
+
+        }
+        public void SalvarAction(object sender,EventArgs args)
+        {
+            bool ErroExiste = false;
+            if (TxtNome.Text == null || TxtNome.Text.Trim().Length <= 0)
+            {
+                ErroExiste = true;
+                DisplayAlert("Erro", "Tarefa não digitada", "Okay");
+            }
+
+            //Já, para a prioridade, 
+            if (Prioridade <= 0)
+            {
+                ErroExiste = true;
+                DisplayAlert("Erro", "Prioridade não escolhida", "Okay");
+            }
+
+            if (ErroExiste == false)
+            {//Salva os dados
+                Tarefa tarefa = new Tarefa();
+                tarefa.Nome = TxtNome.Text.Trim();
+                tarefa.Prioridade = this.Prioridade;
+
+                new GerenciadorTarefa().Salvar(tarefa);
+
+                //TxtNome.Text = new GerenciadorTarefa().Listagem().Count.ToString();
+
+                App.Current.MainPage = new NavigationPage(new Inicio());
+            }
         }
     }
 }
